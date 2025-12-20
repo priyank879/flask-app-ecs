12c8b90e93d8984fe6dad9121bfa58729d1c15e2
priyank879 <priyankbhardwaj87@gmail.com>
2025-12-20 10:16:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Dosto, welcome to DevOps priyank file'
+    return 'Hello Dosto, welcome to DevOps priyank is hero'
 
 @app.route('/health')
 def health():
